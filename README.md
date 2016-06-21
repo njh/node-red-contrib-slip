6028dbb56037668d1c19483fd7d939efabdbfc29
Nicholas Humfrey <njh@aelius.com>
2016-06-21 23:32:13 +0100

    Initial commit for new SLIP node type

---
+++ README.md
@@ -0,0 +1,23 @@
+# node-red-contrib-slip
+
+The *node-red-contrib-slip* module adds [Serial Line Internet Protocol](http://tools.ietf.org/html/rfc1055) packet encoding and decoding support to [Node-RED].
+
+It is useful for packetising messages that are then sent over a stream,
+such as a serial port or TCP socket.
+
+![screenshot](https://github.com/njh/node-red-contrib-slip/raw/master/screenshot.png)
+
+
+## Installation
+
+Change to your node-red installation directory and then run:
+
+    npm install node-red-contrib-slip
+
+You will then be able to see the new SLIP node type added to Node-RED's pallet on the left.
+
+
+Copyright and license
+---------------------
+
+Copyright 2016 Nicholas Humfrey and Nathanaël Lécaudé under [the Apache 2.0 license](LICENSE).
---
+++ slip.js
@@ -0,0 +1,51 @@
+/**
+ * Copyright 2016 Nicholas Humfrey, Nathanaël Lécaudé
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+module.exports = function(RED) {
+    "use strict";
+    var slip = require('slip');
+
+    function SLIP(n) {
+        RED.nodes.createNode(this,n);
+        var node = this;
+        node.mode = n.mode;
+
+        node.decoder = new slip.Decoder({
+            onMessage: function(decoded) {
+                var msg = {payload: new Buffer(decoded)};
+                node.send(msg);
+            },
+            onError: function(msgBuffer, errorMsg) {
+                node.error(errorMsg);
+            }
+        });
+
+        node.on("input", function(msg) {
+            if (node.mode == "encode") {
+                msg.payload = new Buffer(
+                    slip.encode(msg.payload)
+                );
+                node.send(msg);
+            } else if (node.mode == "decode") {
+                node.decoder.decode(
+                    msg.payload
+                );
+            }
+        });
+    }
+
+    RED.nodes.registerType("slip", SLIP);
+};
